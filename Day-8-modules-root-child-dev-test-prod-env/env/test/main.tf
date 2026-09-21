f6cc70ad3c2a95005ea8b81485ecbb34832ced0d
farhatanjumkhan <farhatanjumkhan01@gmail.com>
2026-09-21 19:07:20 +0530

    day-8-module-root-child-dev-test-prod-env

---
+++ Day-8-modules-root-child-dev-test-prod-env/env/test/main.tf
@@ -0,0 +1,13 @@
+module "network" {
+    source = "../../modules/network"
+    vpc_cidr = "10.0.0.0/24"
+    subnet_cidr = "10.0.0.0/24"
+  
+}
+
+module "compute" {
+    source = "../../modules/compute"
+    ami = "ami-0fef201115eefe936"
+    instance_type = "t2.micro"
+    subnet_id = module.network.subnet_id
+}
